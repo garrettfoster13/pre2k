2f687316e4f275b9928bc416e866ed9c65c1e23a
garrettfoster13 <82191679+garrettfoster13@users.noreply.github.com>
2022-06-14 23:02:27 -0700

    Update pre2k.py

--- pre2k.py
+++ pre2k.py
@@ -13,6 +13,10 @@ import ssl
 import sys
 
 
+# credit to @@Oddvarmoe and their blogpost at:
+# https://www.trustedsec.com/blog/diving-into-pre-created-computer-accounts/
+
+
 show_banner = '''
 
                                 ___    __         
