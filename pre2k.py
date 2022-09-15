bb95ca1556392b2ff151def811f43cac7bd4ef0d
garrettfoster13 <82191679+garrettfoster13@users.noreply.github.com>
2022-09-15 16:25:52 -0700

    Update pre2k.py

--- pre2k.py
+++ pre2k.py
@@ -59,7 +59,7 @@ def arg_parse():
     auth_parser.add_argument('-d', action='store', metavar='', required=True, help="Target domain")
     auth_parser.add_argument('-dc-ip', action='store', metavar='', help = "IP address or FQDN of domain controller", required=True)
     auth_parser.add_argument('-ldaps', action="store_true", help='Use LDAPS instead of LDAP')
-    auth_parser.add_argument("-k", "--kerberos", action="store_true", help='Use Kerberos authentication')
+    auth_parser.add_argument("-k", action="store_true", help='Use Kerberos authentication')
     auth_parser.add_argument("-no-pass", action="store_true", help="don't ask for password (useful for -k)")
     auth_parser.add_argument("-hashes", action="store",metavar="LMHASH:NTHASH", help="LM and NT hashes, format is LMHASH:NTHASH",)
     auth_parser.add_argument('-aes', action="store", metavar="hex key", help='AES key to use for Kerberos Authentication (128 or 256 bits)')
